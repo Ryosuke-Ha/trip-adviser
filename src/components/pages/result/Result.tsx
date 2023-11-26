d14f44c4f9c11092a8eafcc54b8325bea918115b
Ryosuke-Ha <ryosuke.0815@icloud.com>
2023-11-26 17:04:41 +0900

    add: initial UI

--- src/components/pages/result/Result.tsx
+++ src/components/pages/result/Result.tsx
@@ -1,6 +1,21 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/reducers";
 import { TripSearchResult } from "../../../models/TripSearchForm";
+import {
+  Avatar,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import {
+  CurrencyYenOutlined,
+  LocationOnOutlined,
+  LuggageOutlined,
+  WarningAmberOutlined,
+} from "@mui/icons-material";
 
 const ResultFelid = () => {
   const results = useSelector(
@@ -10,13 +25,73 @@ const ResultFelid = () => {
     <>
       {results.map((result: TripSearchResult, index: number) => {
         return (
-          <div key={index}>
-            <h2>{result.place}</h2>
-            <p>Reasons to visit: {result.reasons.join(", ")}</p>
-            <p>Recommends: {result.recommends.join(", ")}</p>
-            <p>Prices: {result.prices.join(", ")}</p>
-            <p>Cautions: {result.cautions.join(", ")}</p>
-          </div>
+          <List
+            sx={{
+              width: "100%",
+              maxWidth: 360,
+              bgcolor: "background.paper",
+            }}
+          >
+            <ListItem>
+              <ListItemAvatar>
+                <Typography gutterBottom variant="h5" component="div">
+                  {result.place}
+                </Typography>
+              </ListItemAvatar>
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <LuggageOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="魅力な理由"
+                secondary={result.reasons.join("\n")}
+                style={{ whiteSpace: "pre-line" }}
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <LocationOnOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="おすすめスポット"
+                secondary={result.recommends.join("\n")}
+                style={{ whiteSpace: "pre-line" }}
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <CurrencyYenOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="旅行費目安"
+                secondary={result.prices.join("\n")}
+                style={{ whiteSpace: "pre-line" }}
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <WarningAmberOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="注意点"
+                secondary={result.cautions.join("\n")}
+                style={{ whiteSpace: "pre-line" }}
+              />
+            </ListItem>
+          </List>
         );
       })}
     </>
