fab3092e552259473ce65cac20b63e6ed22af016
Ryosuke-Ha <ryosuke.0815@icloud.com>
2023-11-28 21:20:16 +0900

    add: fix style and add buttons

--- src/components/pages/result/Result.tsx
+++ src/components/pages/result/Result.tsx
@@ -3,6 +3,7 @@ import { RootState } from "../../../store/reducers";
 import { TripSearchResult } from "../../../models/TripSearchForm";
 import {
   Avatar,
+  Button,
   Divider,
   List,
   ListItem,
@@ -16,84 +17,125 @@ import {
   LuggageOutlined,
   WarningAmberOutlined,
 } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const ResultFelid = () => {
   const results = useSelector(
     (state: RootState) => state.formData.searchResult
   );
+
+  const navigate = useNavigate();
+
+  const goHomePage = () => {
+    navigate("/");
+  };
   return (
     <>
-      {results.map((result: TripSearchResult, index: number) => {
-        return (
-          <List
-            sx={{
-              width: "100%",
-              maxWidth: 360,
-              bgcolor: "background.paper",
-            }}
-          >
-            <ListItem>
-              <ListItemAvatar>
-                <Typography gutterBottom variant="h5" component="div">
-                  {result.place}
-                </Typography>
-              </ListItemAvatar>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <LuggageOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="魅力な理由"
-                secondary={result.reasons.join("\n")}
-                style={{ whiteSpace: "pre-line" }}
-              />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <LocationOnOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="おすすめスポット"
-                secondary={result.recommends.join("\n")}
-                style={{ whiteSpace: "pre-line" }}
-              />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <CurrencyYenOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="旅行費目安"
-                secondary={result.prices.join("\n")}
-                style={{ whiteSpace: "pre-line" }}
-              />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <WarningAmberOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="注意点"
-                secondary={result.cautions.join("\n")}
-                style={{ whiteSpace: "pre-line" }}
-              />
-            </ListItem>
-          </List>
-        );
-      })}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          paddingTop: 50,
+        }}
+      >
+        {results.map((result: TripSearchResult, index: number) => {
+          return (
+            <List
+              sx={{
+                width: "100%",
+                maxWidth: 360,
+                bgcolor: "background.paper",
+                borderRadius: 5,
+                paddingRight: 10,
+                border: "aliceblue solid 5px",
+              }}
+            >
+              <ListItem alignItems="center">
+                <ListItemAvatar>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {result.place}
+                  </Typography>
+                </ListItemAvatar>
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <LuggageOutlined />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="魅力な理由"
+                  secondary={result.reasons.join("\n")}
+                  style={{ whiteSpace: "pre-line" }}
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <LocationOnOutlined />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="おすすめスポット"
+                  secondary={result.recommends.join("\n")}
+                  style={{ whiteSpace: "pre-line" }}
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <CurrencyYenOutlined />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="旅行費目安"
+                  secondary={result.prices.join("\n")}
+                  style={{ whiteSpace: "pre-line" }}
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <WarningAmberOutlined />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="注意点"
+                  secondary={result.cautions.join("\n")}
+                  style={{ whiteSpace: "pre-line" }}
+                />
+              </ListItem>
+            </List>
+          );
+        })}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          paddingTop: 50,
+        }}
+      >
+        <Button
+          type="button"
+          variant="contained"
+          style={{ marginLeft: 10, width: "80%", height: "70%" }}
+        >
+          次の3件を取得
+        </Button>
+        <Button
+          type="button"
+          variant="contained"
+          onClick={goHomePage}
+          style={{ marginLeft: 10, width: "80%", height: "70%" }}
+        >
+          条件選択に戻る
+        </Button>
+      </div>
     </>
   );
 };
